3a9f13ca951017a80047de8bb4545151129ce0a3
James Tolen <jtolen1@my.bcit.ca>
2025-11-07 15:03:11 -0800

    Clean up cache folders

--- server.py
+++ server.py
@@ -20,4 +20,6 @@ def home():
     return jsonify({"message": "Waste Classifier API running"}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
